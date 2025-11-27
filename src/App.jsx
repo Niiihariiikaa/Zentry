62c2fc99e8fa4b1fc69b6323c22d672243333ca3
niiihariiikaa <niharikaakashyap@gmail.com>
2025-11-27 20:14:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css';
+import Hero from './components/Hero';
+import About from './components/About';
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
+      <About/>
+      <section className='z-0 min-h-screen bg-blue-500'/>
+    </main>
+  )
+}
+
+export default App
